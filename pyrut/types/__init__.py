b161ba81fdacf4dbfd6a13c060e048943517c3c6
cve-zh00 <bastiang@uc.cl>
2025-06-04 11:52:52 -0400

    delete __init__.py

--- pyrut/types/__init__.py
+++ pyrut/types/__init__.py
@@ -2,7 +2,7 @@ from typing import Annotated
 from pydantic import BeforeValidator
 from functools import partial
 
-from ..pyrut import validate_rut_string
+from pyrut import validate_rut_string
 
 Rut = Annotated[str, BeforeValidator(partial(validate_rut_string, suspicious=False))]
 
